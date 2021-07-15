6bda33bddde861b395ccc337f6e2bb72fed08bbc
1REZ3 <benitopedro21@outlook.com>
2021-07-15 14:17:32 -0300

    commit on main

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.all('*', (req, res) => {
     res.redirect('/index.html')
 })
 
-app.listen(3000, () => console.log('Server running on port 3000....'))
+app.listen(8080, () => console.log('Server running on port 8080....'))
